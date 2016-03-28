b6b2ea662fd7083daffc5709c9aa864053c4f6db
Low WeiLin <lwl1991@gmail.com>
2016-03-28 13:47:06 +0800

    Add good luck message

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ function buildMentionSentence(reviewers) {
 function defaultMessageGenerator(reviewers) {
   return util.format(
     'By analyzing the blame information on this pull request' +
-     ', we identified %s to be%s potential reviewer%s',
+     ', we identified %s to be%s potential reviewer%s. Good luck!',
      buildMentionSentence(reviewers),
      reviewers.length > 1 ? '' : ' a',
      reviewers.length > 1 ? 's' : ''
